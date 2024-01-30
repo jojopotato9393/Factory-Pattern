b32d5331d528fca9ba948a64ed2bf51431e4f749
Joseph Gaughan <jojopotato9393@gmail.com>
2024-01-30 14:45:47 -0600

    yay

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace FactoryPattern
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("WELCOME \n Are you looking for a Car or a Motorcycle");
+            string user = Console.ReadLine();
+           
+            if (user.ToLower() == "car")
+            {
+                string user1 = "four";
+            }
+            else if (user.ToLower() == "motorcycle")
+            {
+                string user1 = "two";
+            }
+            var userCar = VehicleFactory.GetVehivles(user);
+            userCar.Drive();
+            Console.WriteLine($"what do you wnat to put on ur new vehicle");
+            Custom();
+        }
+        public static void Custom()
+        {
+            Console.WriteLine("what color do you want");
+            string color = Console.ReadLine();
+            Console.WriteLine("What size wheels");
+            int wheel = int.Parse(Console.ReadLine());
+            Console.WriteLine($"your new vehicle is {color} with {wheel}in wheels");
+          
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal static class VehicleFactory
+    {
+        public static IVehicle GetVehivles(string choice)
+        {
+            switch (choice.ToLower())
+            {
+                case "four":
+                    return new Car();
+                case "two":
+                    return new Motorcycle();
+                default:
+                    return new Car();
+            }
+        }
+    }
+}
